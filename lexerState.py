32ad9c357f31122a983e307e9cbcbe0daae6d764
NicolasB <nicolasbecma1999@outlook.com>
2023-03-17 17:14:57 -0600

    add lexerState class to helo provide state functionality

---
+++ lexerState.py
@@ -0,0 +1,20 @@
+#create class to store the state of the lexer
+class lexerState:
+    #constructor
+    def __init__(self):
+        #variables used to identify the state
+        self.operand = False
+        self.asignation = False
+        self.comment = False
+        self.variable = False
+        self.number = False
+        self.bracket = False
+
+    #reset the state when change of line
+    def resetState(self):
+        self.operand = False
+        self.asignation = False
+        self.comment = False
+        self.variable = False
+        self.number = False
+        self.bracket = False
